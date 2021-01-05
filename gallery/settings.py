a8432fbe0ed2301e54454761444203216efecaba
claudine Kuradusenge <claudinekuradusenge2@gmail.com>
2021-01-05 13:31:55 +0200

    add changes

--- gallery/settings.py
+++ gallery/settings.py
@@ -15,7 +15,7 @@ import django_heroku
 import dj_database_url
 from decouple import config,Csv
 import django_on_heroku
-django_on_heroku.settings(locals())
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
